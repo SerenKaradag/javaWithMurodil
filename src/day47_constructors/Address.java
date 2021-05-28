00be97f3839ffc411fc62fc1cabbb5d960d95842
SerenKaradag <srn488@gmail.com>
2021-05-27 20:41:48 -0500

    added addressList

--- src/day47_constructors/Address.java
+++ src/day47_constructors/Address.java
@@ -6,10 +6,25 @@ public class Address {
     private String state;
     private String zipCode;
     private String country = "USA";
+    //constructor method- automatically called
+    public Address() {
+        System.out.println("Address constructor");
+        street = "123 unknown st";
+        city = "Unknown";
+        state = "Unknown";
+        zipCode = "00000";
+    }
+    //second constructor, overloaded constructor -> provides shortcut to initialize variables
+    //in same statement: ex: Address ad = new Address("123 java st", "Boston", "MA", "43213");
+    public Address(String street, String city, String state, String zipCode) {
+        setStreet(street);//reuse the code in the setter method
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
 
-    @Override // ANATAION
     public String toString() {
-        return "Address{" + "street='" + street + '\'' + '}';
+        return street + ", " + city + ", " + state + " " + zipCode;
     }
 
     public String getStreet() {
@@ -17,7 +32,12 @@ public class Address {
     }
 
     public void setStreet(String street) {
-        this.street = street;
+        if(street.isEmpty() || street.length() > 50) {
+            System.out.println("ERROR: Invalid street");
+            //System.exit(0);
+        } else {
+            this.street = street;
+        }
     }
 
     public String getCity() {
